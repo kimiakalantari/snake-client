8393ed2fe405619de8841e68ee122e988834cbbf
kimiakalantari <kimiakalantari97@gmail.com>
2023-03-24 12:06:01 -0400

    eslint fixes

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const net = require("net");
 const { IP, PORT } = require('./constants');
 
 
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: IP,// IP address here,
     port: PORT, // PORT number here,

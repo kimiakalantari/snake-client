75686627e257a885d26f8b34fc02c7f99a15802a
kimiakalantari <kimiakalantari97@gmail.com>
2023-03-24 11:52:29 -0400

    completed Sending Messages

--- input.js
+++ input.js
@@ -16,20 +16,44 @@ const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
   }
-  if (key === 'W') {
+  if (key === 'w') {
     connection.write('Move: up');
   }
 
-  if (key === 'S') {
+  if (key === 's') {
     connection.write('Move: down');
   }
 
-  if (key === 'A') {
+  if (key === 'a') {
     connection.write('Move: left');
   }
 
-  if (key === 'D') {
+  if (key === 'd') {
     connection.write('Move: right');
+  }
+
+  if (key === '1') {
+    connection.write('Say: I can\'t be defeated!');
+  }
+
+  if (key === '2') {
+    connection.write('Say: Nomnomnom');
+  }
+
+  if (key === '3') {
+    connection.write('Say: SsSssSs');
+  }
+
+  if (key === '4') {
+    connection.write('Say: LMAO');
+  }
+
+  if (key === '5') {
+    connection.write('Say: bye');
+  }
+
+  if (key === '6') {
+    connection.write('Say: HAHA you died');
   }
 
 };
